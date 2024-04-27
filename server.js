4d4e7b0d6fa0f94198ae67f057f7db08ed5deb52
Min Jung <mj34564n@pace.edu>
2024-04-27 00:01:06 -0400

    Added 1. Get All Products to server.js

--- server.js
+++ server.js
@@ -8,3 +8,16 @@ app.use(express.static("public"));
 app.listen(3000, () => console.log("http://localhost:3000"));
 
 // API routes go here
+
+// 1. Get All Products
+
+app.get('/products', function (req, res) {
+	let query = 'select distinct plant.plantid, plantname, imageurl, price from plant, plantsize where plant.plantid=plantsize.plantid AND plantsize="small"';
+	db.query(query, function (error, results, fields) {
+		if (error) {
+			res.status(500).send('Internal Error');
+		} else {
+			res.json(results);
+		}
+	});
+});
