889372ec02bc3e9a1840d134d76145635972f0ba
Min Jung <mj34564n@pace.edu>
2024-04-30 00:20:08 -0400

    WIP 3. Add Product to Cart 4. Get Cart Details 5. Remove Item from Cart Added 6. Error Page

--- server.js
+++ server.js
@@ -7,17 +7,42 @@ const app = express();
 app.use(express.static("public"));
 app.listen(3000, () => console.log("http://localhost:3000"));
 
+/*// Setup SessionID and Cookies
+const uuid = require('uuid');
+const session = require('express-session');
+
 // API routes go here
+
+app.use(session({ resave: true, saveUninitialized: false, secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+
+// Access the session as req.session
+app.get('/', function(req, res, next) {
+  if (req.session.views) {
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + req.session.views + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session demo. refresh!')
+  }
+}) */
 
 // 1. Get All Products
 
 app.get('/products', function (req, res) {
-	let query = 'select distinct plant.plantid, plantname, imageurl, price from plant, plantsize where plant.plantid=plantsize.plantid AND plantsize="small"';
+	let query = 'select distinct plant.PlantId, PlantName, ImageURL, price as "StartPrice" from plant, plantsize where plant.plantid=plantsize.plantid AND plantsize="small"';
 	db.query(query, function (error, results, fields) {
 		if (error) {
 			res.status(500).send('Internal Error');
 		} else {
-			res.json(results);
+			if (results.length === 0) {
+				res.status(500).send('Not Found');
+			} else {
+				res.json(results);
+				
+			}
 		}
 	});
 });
@@ -31,7 +56,149 @@ app.get('/products/:productid', function (req, res) {
 		if (error) {
 			res.status(500).send('Internal Error');
 		} else {
-			res.json(results);
+			if (results.length === 0) {
+				res.status(500).send('Not Found');
+			} else {
+				res.json(results);
+			}
 		}
 	});
+});
+
+/* // 3. Add Product to Cart
+
+app.post('/cart/items', function (req, res) {
+	// Get PlantId from the URL query parameter
+	const urlParams = new URLSearchParams(window.location.search);
+	const plantId = parseInt(urlParams.get("id"));
+	console.log(urlParams);
+	console.log(plantId);
+	
+	let query1 = 'select distinct plant.PlantId, PlantName, ImageURL, price as "StartPrice" from plant, plantsize where plant.plantid=plantsize.plantid AND plantsize="small"';
+	db.query1(query1, function (error, results, fields) {
+		if (error) {
+			res.status(500).send('Internal Error');
+		} else {
+			const plantList = res.json(result);
+		}
+	});
+	
+	// if plantid is same as the plant id in the platlist
+	const desiredPlant = plantList.find((plant) => plant.PlantId === plantId);
+	// get
+	if (desiredPlant) {
+		console.log(desiredPlant.PlantName);
+		// Set plant name and start price
+		plantItem.querySelector("h2").textContent = desiredPlant.PlantName;
+		plantItem.querySelector("p").textContent =
+			`Available from $${desiredPlant.StartPrice}`;
+		// Set plant image
+		plantItem.querySelector("img").src = desiredPlant.ImageURL;
+		plantItem.querySelector("img").alt = `${desiredPlant.PlantName} plant`;
+	} else {
+		console.log(`Plant with PlantId ${plantId} not found.`);
+	}
+	
+	const size = req.params['size'];
+	let query2 = 'select plantname, plantsize, price from plant, plantsize where plantsize.plantid=plant.plantid AND plant.plantid=' + plantId + ' AND plantsize="small"';
+	db.query2(query2, function (error, results, fields) {
+		if (error) {
+			res.status(500).send('Internal Error');
+		} else {
+			res.json(results);
+			res.send("Was Added to the Cart");
+			app.use(session({
+				genid: function (req) {
+				console.log(uuid.v4());
+				},
+				secret: 'keyboard cat',
+				saveUninitialized: false,
+				resave: true,
+				cookie: { secure: true }
+			}));
+		}
+	});
+
+	app.get('/cart/items', function(req, res) {
+		if (error) {
+			app.use(session({
+				genid: function (req) {
+				const sessionID = uuid.v4();
+				console.log(sessionID);
+				},
+				secret: 'keyboard cat',
+				saveUninitialized: false,
+				resave: true,
+				cookie: { secure: true }
+			}));
+			app.use(session({
+				genid: function (req) {
+				const cartItemID = uuid.v4();
+				console.log(sessionID);
+				},
+				secret: 'keyboard cat',
+				saveUninitialized: false,
+				resave: true,
+				cookie: { secure: true }
+			}));
+			let query3 = 'insert into cartitems (cartItemID, plantID, PlantSizeID, sessionID) values ' +  ;
+			db.query3(error, results, fields) {
+				res.send("Was Added To The Cart")
+			}
+		}
+	// if cart is empty
+		// generate a session ID
+			/*app.use(session({
+				genid: function (req) {
+				return genuuid()
+				},
+				secret: 'keyboard cat',
+				resave: false,
+				cookie: { secure: false }
+			})) */
+
+	// else
+		// use previously generated session ID
+			// 'insert into table cartitems (cartitemid, sessionid,) */
+
+	
+// 4. Get Cart Details
+
+app.get('/cart/items', function (req, res) {
+	let query = 'select * from cartitem';
+	db.query(query, function (error, results, fields) {
+		if (error) {
+			res.status(500).send('Internal Error');
+		} else {
+			if (results.length === 0) {
+				res.status(500).send('Not Found');
+			res.json(results);
+			}
+		}
+	});
+});
+
+// 5. Remove Item from Cart
+
+app.delete('/cart/items/:cartitemid', function (req, res) {
+	const id = req.params['cartitemid'];
+	let query = 'delete from cartitem where cartitemid =' + cartitemid;
+	db.query(query, function (error, results, fields) {
+		if (error) {
+			res.status(500).send('Internal Error');
+		} else {
+			if (results.length === 0) {
+				res.status(500).send('Not Found');
+			res.json(results);
+			res.post('Item removed.')
+			}
+		}
+	});
+});
+	
+// 6. Error Page
+
+app.get('/error', function (req, res) {
+	res.set("Content-Type", "text/plain");
+	res.status(400).send('Error, Bad Request!');
 });
