0a742c2daaaae393dd48649576f1a45fc485ac2d
Ttan-24 <tanviprakash24@gmail.com>
2024-05-06 20:55:49 -0400

    Remove cart item from the cart list functionality - images > Added the remove-icon button - cart.js > Added the remove cart item functionality, update cart count and total cart price - cart.css > css style for the remove button and buy now link -  cart.html > add html tags for the remove button type and buy now - server.js > Modified back the query by adding Plant.PlantID in the get cart item endpoint

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -2,11 +2,17 @@
 const cartListElement = document.getElementById("cart-list");
 const cartItemTemplate = document.getElementById("cart-item");
 
+// Get cart button element
+const cartButtonElement = document.getElementById("cart-button");
+
 // Get cart header
 const totalCartItemElement = document.querySelector("main p");
 
 // Fetch cart list from server
 const cartList = await (await fetch("/cart/items")).json();
+
+// cart counter for the cart button
+let cartCount = cartList.length;
 
 // counter to add up the total items in the cart
 let totalCartPrice = 0;
@@ -14,7 +20,7 @@ let totalCartPrice = 0;
 // Iterate through the cart list to get the cart items
 for (const cartItem of cartList) {
   // Clone cart item template
-  const cartItemElement = cartItemTemplate.content.cloneNode(true);
+  let cartItemElement = cartItemTemplate.content.cloneNode(true);
 
   // Sum cart prices in the cartlist
   totalCartPrice += cartItem.Price;
@@ -33,9 +39,32 @@ for (const cartItem of cartList) {
   cartImageElement.src = cartItem.ImageURL;
   cartImageElement.alt = `${cartItem.PlantName} plant`;
 
+  // delete cart item from the cart list
+  cartItemElement
+    .querySelector("form")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      // call delete cart endpoint
+      await fetch(`/cart/items/${cartItem.CartItemID}`, {
+        method: "DELETE",
+      });
+      cartListElement.removeChild(cartItemElement);
+
+      // decrement the cart counter and total cart price
+      cartCount--;
+      totalCartPrice -= cartItem.Price;
+
+      // set the new cart counter and total cart price
+      totalCartItemElement.textContent = `With a total of $${totalCartPrice}`;
+      cartButtonElement.querySelector("span").textContent = cartCount;
+    });
+
   // Append plant item to list
   cartListElement.appendChild(cartItemElement);
+  cartItemElement = cartListElement.lastElementChild;
 }
 
 // Displays the total cart price from the cart list
 totalCartItemElement.textContent = `With a total of $${totalCartPrice}`;
+
